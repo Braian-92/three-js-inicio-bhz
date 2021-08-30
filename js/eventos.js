0f3c9ab5716f651b5ea9f57cb646a7740a31194e
Braian Zamudio <zamudiobraianhernan@gmail.com>
2021-08-30 03:47:55 -0300

    ejeAyuda

--- js/eventos.js
+++ js/eventos.js
@@ -8,6 +8,11 @@ const material = new THREE.MeshBasicMaterial({color:0xff0000});
 const malla = new THREE.Mesh(geometria, material);
 escena.add(malla);
 
+//! eje de ayuda 
+
+const ejeAyuda = new THREE.AxesHelper();
+escena.add(ejeAyuda);
+
 //# dimenciones
 const dimen = {
 	ancho: 800,
@@ -18,9 +23,9 @@ const dimen = {
 // const camara = new THREE.PerspectiveCamera(75, dimen.ancho, dimen.alto);
 const camara = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-camara.position.z = 2;
-// camara.position.x = 3;
-// camara.position.y = 3;
+camara.position.z = 3;
+// camara.position.x = 1;
+// camara.position.y = 1;
 escena.add(camara);
 
 // //# Renderizar
