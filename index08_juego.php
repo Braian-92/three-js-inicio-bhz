bb5d3a89e18719d622ce283110d99124482c5f3d
Braian-92 <zamudiobraianhernan@gmail.com>
2023-03-23 02:41:39 -0300

    carrito en la pista circular

--- index08_juego.php
+++ index08_juego.php
@@ -20,7 +20,10 @@
       var scene = new THREE.Scene();
       scene.background = new THREE.Color(0x223344);
 
+      let enJuego = false
       let frameCount = 0
+      let vueltasPorSegundo = 0.2;
+      let tiempoInicio = Date.now();
 
       //add camera
       let zoom = 0.2;
@@ -44,6 +47,40 @@
       renderer.setSize(window.innerWidth, window.innerHeight); //!  ajuste de dimenciones del canvas	
       document.body.appendChild(renderer.domElement); //! agregarlo al DOM HTML
 
+      function iniciar() {
+           if (enJuego) {
+                return
+           }
+           enJuego = true
+           jugador.ACELERAR = 0
+           jugador.angulo = PI
+           // jugador2.angulo = -PI
+           // jugador3.angulo = -PI / 2
+           // jugador4.angulo = PI
+           // document.getElementById("info").style.visibility = "hidden"
+      }
+
+      document.addEventListener("keydown", (evnt) => {
+           switch (evnt.keyCode) {
+                case KEY_UP:
+                case KEY_W:
+                     jugador.ACELERAR = 1
+                     break;
+                case KEY_DOWN:
+                case KEY_S:
+                     jugador.ACELERAR = -1
+                     break
+                case KEY_ENTER:
+                     iniciar();
+                     break
+                default:
+                     break;
+           }
+      })
+
+      document.addEventListener("keyup", (evnt) => {
+           jugador.ACELERAR = 0
+      })
       //add geometry
       // var geometry = new THREE.BoxGeometry(30,30,30,5,5,5);
       // var material = new THREE.MeshBasicMaterial({
@@ -56,6 +93,7 @@
       let jugador = Car();
       jugador.rotation.z = -PI/4
       scene.add(jugador);
+      scene.add(pista())
 
       const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
       scene.add(ambientLight);
@@ -74,6 +112,8 @@
       // controls.maxPolarAngle = Math.PI;
       controls.screenSpacePanning = true;
 
+      iniciar();
+
       window.addEventListener('resize', redimensionar);
 
       function redimensionar(){
@@ -84,8 +124,44 @@
       }
 
       function actualizar(){
-      	jugador.rotation.z = -(frameCount/100) - PI / 4;
-      	frameCount++;
+      	const tiempo_transcurrido = (Date.now() - tiempoInicio) / 1000;
+         if (enJuego) {
+              {//Dibujado del primer jugador
+                   const girar = 2 * PI * tiempo_transcurrido * vueltasPorSegundo * (2 ** jugador.ACELERAR)
+                   jugador.angulo += girar
+                   let p = trayectoriaCircular(
+                         -150,
+                         0,
+                         270 + 40,
+                         jugador.angulo
+                   )
+                   jugador.position.x = p.x
+                   jugador.position.y = p.y
+                   jugador.rotation.z = jugador.angulo + PI / 2
+              }
+
+              let carriles = [40, 40, -40]
+              let velocidades = [1.5, 1.5, 3]
+              // let obstaculos = [jugador2, jugador3, jugador4]
+              // for (let i = 0; i < obstaculos.length; i++) {
+              //      let jugadorObstaculo = obstaculos[i]
+              //      jugadorObstaculo.angulo -= velocidades[i] * 2 * PI * tiempo_transcurrido * vueltasPorSegundo
+              //      let p = trayectoriaCircular(
+              //           x = 150,
+              //           y = 0,
+              //           r = 270 + carriles[i],
+              //           a = jugadorObstaculo.angulo
+              //      )
+              //      jugadorObstaculo.position.x = p.x
+              //      jugadorObstaculo.position.y = p.y
+              //      jugadorObstaculo.rotation.z = jugadorObstaculo.angulo - PI / 2
+              //      if (distancia3D(jugador.position, jugadorObstaculo.position) < 35) {
+              //           finalizar()
+              //      }
+              // }
+         }
+         // document.getElementById("Puntaje").innerHTML = int(jugador.angulo / (2 * PI))
+         tiempoInicio = Date.now()
       }
       
 
@@ -190,6 +266,80 @@
 
        return g
      }
+
+     function pista() {
+        const planeGeometry = new THREE.PlaneBufferGeometry(1280, 1000)
+        const material = new THREE.MeshPhongMaterial({ map: textura() })
+        const plano = new THREE.Mesh(planeGeometry, material)
+        plano.receiveShadow = true;
+        return plano
+
+        function textura() {
+             const canvas = document.createElement("canvas")
+             canvas.width = 1280
+             canvas.height = 1000
+             const ctx = canvas.getContext("2d")
+
+             //Color del pasto
+             ctx.fillStyle = "limegreen"
+             ctx.fillRect(0, 0, 1280, 1000)
+
+             //Relieve del pasto
+             ctx.strokeStyle = "green"
+             ctx.save()
+             ctx.translate(1280 / 2, canvas.height / 2)
+             ctx.lineWidth = 150
+             {
+                  ctx.beginPath();
+                  ctx.arc(-150, -15, 270, 0, 2 * PI);
+                  ctx.stroke()
+             }
+             {
+                  ctx.beginPath();
+                  ctx.arc(150, -15, 270, 0, 2 * PI);
+                  ctx.stroke()
+             }
+             ctx.restore()
+
+             //Concreto de la carretera
+             ctx.strokeStyle = "darkslategray"
+             ctx.save()
+             ctx.translate(1280 / 2, canvas.height / 2)
+             ctx.lineWidth = 150
+             {
+                  ctx.beginPath();
+                  ctx.arc(-5 * 30, 0, 9 * 30, 0, 2 * PI);
+                  ctx.stroke()
+             }
+             {
+                  ctx.beginPath();
+                  ctx.arc(5 * 30, 0, 9 * 30, 0, 2 * PI);
+                  ctx.stroke()
+             }
+             ctx.restore()
+
+             //Lineas de los carriles
+             ctx.lineWidth = 2
+             ctx.strokeStyle = "white"
+             ctx.setLineDash([10, 14])
+
+             ctx.save()
+             ctx.translate(1280 / 2, canvas.height / 2)
+             {
+                  ctx.beginPath();
+                  ctx.arc(-5 * 30, 0, 9 * 30, 0, 2 * PI);
+                  ctx.stroke()
+             }
+             {
+                  ctx.beginPath();
+                  ctx.arc(5 * 30, 0, 9 * 30, 0, 2 * PI);
+                  ctx.stroke()
+             }
+             ctx.restore()
+
+             return new THREE.CanvasTexture(canvas)
+        }
+   }
     </script>
   </body>
 </html>
