6e0f51b7e6c47b501d8bae05d97d5fc063435b0c
Braian-92 <zamudiobraianhernan@gmail.com>
2023-04-15 03:20:11 -0300

    disparo basico

--- index21.php
+++ index21.php
@@ -83,6 +83,14 @@
   renderer.setSize(pantalla.clientWidth , pantalla.clientHeight);
   pantalla.appendChild(renderer.domElement);
 
+  var controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.5;
+
+  controls.maxPolarAngle = Math.PI;
+
+  controls.screenSpacePanning = true;
+
   window.addEventListener('resize', function() {
     // w = container.clientWidth;
     // h = container.clientHeight;
@@ -273,7 +281,218 @@
         vehicle.setSteeringValue(keyup ? 0 : maxSteerVal, 2);
         vehicle.setSteeringValue(keyup ? 0 : maxSteerVal, 3);
         break;
+      case 32: // espacio
+        disparo();
+        break;
     }
+  }
+
+  let esfera = new CANNON.Sphere(0.5);
+  let caja = new CANNON.Box(new CANNON.Vec3(0.5,0.5,0.5));
+
+  let disparos = [];
+
+  function disparo( sphere=true ){
+    const material = new CANNON.Material();
+    const body = new CANNON.Body({ mass: 5, material: material });
+    if (sphere){
+      body.addShape(esfera);
+    }else{
+      body.addShape(caja);
+    }
+        
+    const x = Math.random()*0.3 + 1;
+    body.position.set((sphere) ? -x : x, 5, 0);
+
+    // body.position.set(vehicle.position.x, vehicle.position.y, vehicle.position.z);
+    body.linearDamping = 0.01;
+    disparos.push(body);
+    world.addBody(body);
+    addVisual(body, 'name'+Math.random());
+        
+        
+    const material_ground = new CANNON.ContactMaterial(groundMaterial, material, { friction: 0.0, restitution: (sphere) ? 0.9 : 0.3 });
+    
+    world.addContactMaterial(material_ground);
+  }
+
+  function addVisual(body, name, castShadow=true, receiveShadow=true){
+    body.name = name;
+    let mesh = agregarMaya(body, castShadow, receiveShadow);
+    body.threemesh = mesh;
+          mesh.castShadow = castShadow;
+          mesh.receiveShadow = receiveShadow;
+    scene.add(mesh);
+  }
+
+  function agregarMaya(body, castShadow, receiveShadow){
+    const obj = new THREE.Object3D();
+    let material = new THREE.MeshLambertMaterial({color:0x888888});
+    const game = this;
+    let index = 0;
+
+    let particleGeo = new THREE.SphereGeometry( 1, 16, 8 );
+    let particleMaterial = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
+
+    let s = {
+      stepFrequency: 60,
+      quatNormalizeSkip: 2,
+      quatNormalizeFast: true,
+      gx: 0,
+      gy: 0,
+      gz: 0,
+      iterations: 3,
+      tolerance: 0.0001,
+      k: 1e6,
+      d: 3,
+      scene: 0,
+      paused: false,
+      rendermode: "solid",
+      constraints: false,
+      contacts: false,  // Contact points
+      cm2contact: false, // center of mass to contact points
+      normals: false, // contact normals
+      axes: false, // "local" frame axes
+      particleSize: 0.1,
+      shadows: false,
+      aabbs: false,
+      profiling: false,
+      maxSubSteps:3
+    };
+    
+    body.shapes.forEach (function(shape){
+      let mesh;
+      let geometry;
+      let v0, v1, v2;
+
+      switch(shape.type){
+
+      case CANNON.Shape.types.SPHERE:
+        const sphere_geometry = new THREE.SphereGeometry( shape.radius, 8, 8);
+        mesh = new THREE.Mesh( sphere_geometry, material );
+        break;
+
+      case CANNON.Shape.types.PARTICLE:
+        mesh = new THREE.Mesh( particleGeo, particleMaterial );
+        
+        mesh.scale.set(s.particleSize,s.particleSize,s.particleSize);
+        break;
+
+      case CANNON.Shape.types.PLANE:
+        geometry = new THREE.PlaneGeometry(10, 10, 4, 4);
+        mesh = new THREE.Object3D();
+        const submesh = new THREE.Object3D();
+        const ground = new THREE.Mesh( geometry, material );
+        ground.scale.set(100, 100, 100);
+        submesh.add(ground);
+
+        mesh.add(submesh);
+        break;
+
+      case CANNON.Shape.types.BOX:
+        const box_geometry = new THREE.BoxGeometry(  shape.halfExtents.x*2,
+                              shape.halfExtents.y*2,
+                              shape.halfExtents.z*2 );
+        mesh = new THREE.Mesh( box_geometry, material );
+        break;
+
+      case CANNON.Shape.types.CONVEXPOLYHEDRON:
+        const geo = new THREE.Geometry();
+
+        // Add vertices
+        shape.vertices.forEach(function(v){
+          geo.vertices.push(new THREE.Vector3(v.x, v.y, v.z));
+        });
+
+        shape.faces.forEach(function(face){
+          // add triangles
+          const a = face[0];
+          for (let j = 1; j < face.length - 1; j++) {
+            const b = face[j];
+            const c = face[j + 1];
+            geo.faces.push(new THREE.Face3(a, b, c));
+          }
+        });
+        geo.computeBoundingSphere();
+        geo.computeFaceNormals();
+        mesh = new THREE.Mesh( geo, material );
+        break;
+
+      case CANNON.Shape.types.HEIGHTFIELD:
+        geometry = new THREE.Geometry();
+
+        v0 = new CANNON.Vec3();
+        v1 = new CANNON.Vec3();
+        v2 = new CANNON.Vec3();
+        for (let xi = 0; xi < shape.data.length - 1; xi++) {
+          for (let yi = 0; yi < shape.data[xi].length - 1; yi++) {
+            for (let k = 0; k < 2; k++) {
+              shape.getConvexTrianglePillar(xi, yi, k===0);
+              v0.copy(shape.pillarConvex.vertices[0]);
+              v1.copy(shape.pillarConvex.vertices[1]);
+              v2.copy(shape.pillarConvex.vertices[2]);
+              v0.vadd(shape.pillarOffset, v0);
+              v1.vadd(shape.pillarOffset, v1);
+              v2.vadd(shape.pillarOffset, v2);
+              geometry.vertices.push(
+                new THREE.Vector3(v0.x, v0.y, v0.z),
+                new THREE.Vector3(v1.x, v1.y, v1.z),
+                new THREE.Vector3(v2.x, v2.y, v2.z)
+              );
+              var i = geometry.vertices.length - 3;
+              geometry.faces.push(new THREE.Face3(i, i+1, i+2));
+            }
+          }
+        }
+        geometry.computeBoundingSphere();
+        geometry.computeFaceNormals();
+        mesh = new THREE.Mesh(geometry, material);
+        break;
+
+      case CANNON.Shape.types.TRIMESH:
+        geometry = new THREE.Geometry();
+
+        v0 = new CANNON.Vec3();
+        v1 = new CANNON.Vec3();
+        v2 = new CANNON.Vec3();
+        for (let i = 0; i < shape.indices.length / 3; i++) {
+          shape.getTriangleVertices(i, v0, v1, v2);
+          geometry.vertices.push(
+            new THREE.Vector3(v0.x, v0.y, v0.z),
+            new THREE.Vector3(v1.x, v1.y, v1.z),
+            new THREE.Vector3(v2.x, v2.y, v2.z)
+          );
+          var j = geometry.vertices.length - 3;
+          geometry.faces.push(new THREE.Face3(j, j+1, j+2));
+        }
+        geometry.computeBoundingSphere();
+        geometry.computeFaceNormals();
+        mesh = new THREE.Mesh(geometry, MutationRecordaterial);
+        break;
+
+      default:
+        throw "Visual type not recognized: "+shape.type;
+      }
+
+      mesh.receiveShadow = receiveShadow;
+      mesh.castShadow = castShadow;
+            
+            mesh.traverse( function(child){
+                if (child.isMesh){
+                    child.castShadow = castShadow;
+          child.receiveShadow = receiveShadow;
+                }
+            });
+
+      var o = body.shapeOffsets[index];
+      var q = body.shapeOrientations[index++];
+      mesh.position.set(o.x, o.y, o.z);
+      mesh.quaternion.set(q.x, q.y, q.z, q.w);
+
+      obj.add(mesh);
+    });
+
+    return obj;
   }
 
   window.addEventListener('keydown', navigate)
