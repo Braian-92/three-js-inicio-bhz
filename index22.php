862199d5e0633911a5b488b469bb4d4b7aad9c04
Braian-92 <zamudiobraianhernan@gmail.com>
2023-04-16 01:18:05 -0300

    base disparo limpio

--- index22.php
+++ index22.php
@@ -63,10 +63,10 @@
 
   const pantalla = document.querySelector('#pantalla');
 
-  var container = document.querySelector('body'),
-      w = container.clientWidth,
-      h = container.clientHeight,
-      scene = new THREE.Scene(),
+
+    
+    
+      let scene = new THREE.Scene(),
       // camera = new THREE.PerspectiveCamera(75, w/h, 0.001, 100),
       camera = new THREE.PerspectiveCamera(
         75,
@@ -77,8 +77,7 @@
   camera.position.set(0, 1, -10);
   camera.lookAt(0,0,0);
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(w, h);
-  // container.appendChild(renderer.domElement);
+
 
   renderer.setSize(pantalla.clientWidth , pantalla.clientHeight);
   pantalla.appendChild(renderer.domElement);
@@ -92,8 +91,8 @@
   controls.screenSpacePanning = true;
 
   window.addEventListener('resize', function() {
-    // w = container.clientWidth;
-    // h = container.clientHeight;
+  
+  
     // camera.aspect = w/h;
     // camera.updateProjectionMatrix();
     // renderer.setSize(w, h);
@@ -103,11 +102,11 @@
     renderer.setSize(pantalla.clientWidth, pantalla.clientHeight);
   })
 
-  var geometry = new THREE.PlaneGeometry(10, 10, 10);
-  var material = new THREE.MeshBasicMaterial({color: 0x14EA2AFF, side: THREE.DoubleSide});
-  var plane = new THREE.Mesh(geometry, material);
-  plane.rotation.x = Math.PI/2;
-  scene.add(plane);
+  // var geometry = new THREE.PlaneGeometry(10, 10, 10);
+  // var material = new THREE.MeshBasicMaterial({color: 0x14EA2AFF, side: THREE.DoubleSide});
+  // var plane = new THREE.Mesh(geometry, material);
+  // plane.rotation.x = Math.PI/2;
+  // scene.add(plane);
 
   var sunlight = new THREE.DirectionalLight(0xffffff, 1.0);
   sunlight.position.set(-10, 10, 0);
@@ -117,210 +116,58 @@
   * Physics
   **/
 
-  var world = new CANNON.World();
-  world.broadphase = new CANNON.SAPBroadphase(world);
+  const world = new CANNON.World();
+  let fixedTimeStep = 1.0/60.0;
+  let damping = 0.01;
+  
+  world.broadphase = new CANNON.NaiveBroadphase();
   world.gravity.set(0, -10, 0);
-  world.defaultContactMaterial.friction = 0;
+  // debugRenderer = new THREE.CannonDebugRenderer(scene, world);
+  
+  const groundShape = new CANNON.Plane();
+      const groundMaterial = new CANNON.Material();
+  const groundBody = new CANNON.Body({ mass: 0, material: groundMaterial });
+  groundBody.quaternion.setFromAxisAngle( new CANNON.Vec3(1,0,0), -Math.PI/2);
+  groundBody.addShape(groundShape);
+  world.addBody(groundBody);
+      
+  addVisual(groundBody, 'ground', false, true);
+      
+  let shapes = {};
+  shapes.sphere = new CANNON.Sphere(0.5);
+  shapes.box = new CANNON.Box(new CANNON.Vec3(0.5,0.5,0.5));
 
-  var groundMaterial = new CANNON.Material('groundMaterial');
-  var wheelMaterial = new CANNON.Material('wheelMaterial');
-  var wheelGroundContactMaterial = new CANNON.ContactMaterial(wheelMaterial, groundMaterial, {
-      friction: 0.3,
-      restitution: 0,
-      contactEquationStiffness: 1000,
-  });
 
-  world.addContactMaterial(wheelGroundContactMaterial);
-
-  // car physics body
-  var chassisShape = new CANNON.Box(new CANNON.Vec3(1, 0.3, 2));
-  var chassisBody = new CANNON.Body({mass: 150});
-  chassisBody.addShape(chassisShape);
-  chassisBody.position.set(0, 0.2, 0);
-  chassisBody.angularVelocity.set(0, 0, 0); // initial velocity
-
-  // car visual body
-  var geometry = new THREE.BoxGeometry(2, 1, 4); // double chasis shape
-  var material = new THREE.MeshBasicMaterial({color: 0xffff00, side: THREE.DoubleSide});
-  var box = new THREE.Mesh(geometry, material);
-  scene.add(box);
-
-  // parent vehicle object
-  let vehicle = new CANNON.RaycastVehicle({
-    chassisBody: chassisBody,
-    indexRightAxis: 0, // x
-    indexUpAxis: 1, // y
-    indexForwardAxis: 2, // z
-  });
-
-  // wheel options
-  var options = {
-    radius: 0.6,
-    directionLocal: new CANNON.Vec3(0, -1, 0),
-    suspensionStiffness: 45,
-    suspensionRestLength: 0.4,
-    frictionSlip: 5,
-    dampingRelaxation: 2.3,
-    dampingCompression: 4.5,
-    maxSuspensionForce: 200000,
-    rollInfluence:  0.01,
-    axleLocal: new CANNON.Vec3(-1, 0, 0),
-    chassisConnectionPointLocal: new CANNON.Vec3(1, 1, 0),
-    maxSuspensionTravel: 0.25,
-    customSlidingRotationalSpeed: -30,
-    useCustomSlidingRotationalSpeed: true,
-  };
-
-  var axlewidth = 0.7;
-  options.chassisConnectionPointLocal.set(axlewidth, 0, -1);
-  vehicle.addWheel(options);
-
-  options.chassisConnectionPointLocal.set(-axlewidth, 0, -1);
-  vehicle.addWheel(options);
-
-  options.chassisConnectionPointLocal.set(axlewidth, 0, 1);
-  vehicle.addWheel(options);
-
-  options.chassisConnectionPointLocal.set(-axlewidth, 0, 1);
-  vehicle.addWheel(options);
-
-  vehicle.addToWorld(world);
-
-  // car wheels
-  var wheelBodies = [],
-      wheelVisuals = [];
-  vehicle.wheelInfos.forEach(function(wheel) {
-    var shape = new CANNON.Cylinder(wheel.radius, wheel.radius, wheel.radius / 2, 20);
-    var body = new CANNON.Body({mass: 1, material: wheelMaterial});
-    var q = new CANNON.Quaternion();
-    q.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), Math.PI / 2);
-    body.addShape(shape, new CANNON.Vec3(), q);
-    wheelBodies.push(body);
-    // wheel visual body
-    var geometry = new THREE.CylinderGeometry( wheel.radius, wheel.radius, 0.4, 32 );
-    var material = new THREE.MeshPhongMaterial({
-      color: 0xd0901d,
-      emissive: 0xaa0000,
-      side: THREE.DoubleSide,
-      flatShading: true,
-    });
-    var cylinder = new THREE.Mesh(geometry, material);
-    cylinder.geometry.rotateZ(Math.PI/2);
-    wheelVisuals.push(cylinder);
-    scene.add(cylinder);
-  });
-
-  // update the wheels to match the physics
-  world.addEventListener('postStep', function() {
-    for (var i=0; i<vehicle.wheelInfos.length; i++) {
-      vehicle.updateWheelTransform(i);
-      var t = vehicle.wheelInfos[i].worldTransform;
-      // update wheel physics
-      wheelBodies[i].position.copy(t.position);
-      wheelBodies[i].quaternion.copy(t.quaternion);
-      // update wheel visuals
-      wheelVisuals[i].position.copy(t.position);
-      wheelVisuals[i].quaternion.copy(t.quaternion);
-    }
-  });
-
-  var q = plane.quaternion;
-  var planeBody = new CANNON.Body({
-    mass: 0, // mass = 0 makes the body static
-    material: groundMaterial,
-    shape: new CANNON.Plane(),
-    quaternion: new CANNON.Quaternion(-q._x, q._y, q._z, q._w)
-  });
-  world.addBody(planeBody)
-
-  /**
-  * Main
-  **/
-
-  function updatePhysics() {
-    world.step(1/60);
-    // update the chassis position
-    box.position.copy(chassisBody.position);
-    box.quaternion.copy(chassisBody.quaternion);
-  }
-  var vec3 = new THREE.Vector3();
   function render() {
     requestAnimationFrame(render);
+    world.step(fixedTimeStep);
+    updateBodies(world);
     renderer.render(scene, camera);
-    vec3.subVectors(camera.position, chassisBody.position);
-    controls.object.position.copy(chassisBody.position).add(vec3);
-    controls.target.copy(chassisBody.position);
-    controls.update();
-    updatePhysics();
   }
 
-  function navigate(e) {
-    if (e.type != 'keydown' && e.type != 'keyup') return;
-    var keyup = e.type == 'keyup';
-    vehicle.setBrake(0, 0);
-    vehicle.setBrake(0, 1);
-    vehicle.setBrake(0, 2);
-    vehicle.setBrake(0, 3);
-
-    var engineForce = 800,
-        maxSteerVal = 0.3;
-    switch(e.keyCode) {
-
-      case 38: // forward
-        vehicle.applyEngineForce(keyup ? 0 : -engineForce, 2);
-        vehicle.applyEngineForce(keyup ? 0 : -engineForce, 3);
-        break;
-
-      case 40: // backward
-        vehicle.applyEngineForce(keyup ? 0 : engineForce, 2);
-        vehicle.applyEngineForce(keyup ? 0 : engineForce, 3);
-        break;
-
-      case 39: // right
-        vehicle.setSteeringValue(keyup ? 0 : -maxSteerVal, 2);
-        vehicle.setSteeringValue(keyup ? 0 : -maxSteerVal, 3);
-        break;
-
-      case 37: // left
-        vehicle.setSteeringValue(keyup ? 0 : maxSteerVal, 2);
-        vehicle.setSteeringValue(keyup ? 0 : maxSteerVal, 3);
-        break;
-      case 32: // espacio
-        disparo();
-        break;
-    }
-  }
-
-  let esfera = new CANNON.Sphere(0.5);
-  let caja = new CANNON.Box(new CANNON.Vec3(0.5,0.5,0.5));
-
-  let disparos = [];
-
-  function disparo( sphere=true ){
+  function addBody( sphere=true ){
     const material = new CANNON.Material();
     const body = new CANNON.Body({ mass: 5, material: material });
     if (sphere){
-      body.addShape(esfera);
+      body.addShape(shapes.sphere);
     }else{
-      body.addShape(caja);
+      body.addShape(shapes.box);
     }
         
     const x = Math.random()*0.3 + 1;
-    body.position.set(10, 10, 10);
-    // body.position.set((sphere) ? -x : x, 5, 0);
-    // body.position.x =;
-
-    // body.position.set(vehicle.position.x, vehicle.position.y, vehicle.position.z);
-    body.linearDamping = 0.01;
-    disparos.push(body);
+    body.position.set((sphere) ? -x : x, 5, 0);
+    body.linearDamping = damping;
     world.addBody(body);
-    addVisual(body, 'name'+Math.random());
         
+    addVisual(body, (sphere) ? 'sphere' : 'box', true, false);
         
+    // Create contact material behaviour
     const material_ground = new CANNON.ContactMaterial(groundMaterial, material, { friction: 0.0, restitution: (sphere) ? 0.9 : 0.3 });
     
     world.addContactMaterial(material_ground);
   }
+
+  
 
   function addVisual(body, name, castShadow=true, receiveShadow=true){
     body.name = name;
@@ -501,8 +348,27 @@
     return obj;
   }
 
-  window.addEventListener('keydown', navigate)
-  window.addEventListener('keyup', navigate)
+  function updateBodies(world){
+    world.bodies.forEach( function(body){
+        if ( body.threemesh != undefined){
+          body.threemesh.position.copy(body.position);
+          body.threemesh.quaternion.copy(body.quaternion);
+        }
+    });
+  }
+
+  function navigate(e) {
+    if (e.type != 'keydown' && e.type != 'keyup') return;
+    var keyup = e.type == 'keyup';
+    switch(e.keyCode) {
+      case 32: // espacio
+        addBody();
+        break;
+    }
+  }
+
+  window.addEventListener('keydown', navigate);
+  window.addEventListener('keyup', navigate);
 
   render();
   
