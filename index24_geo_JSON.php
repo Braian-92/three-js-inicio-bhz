fc894420ca722b1062f6c09f5addb919cfef7a19
Braian-92 <zamudiobraianhernan@gmail.com>
2023-04-28 00:49:00 -0300

    mapa esférico

---
+++ index24_geo_JSON.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Parcel Sandbox</title>
+    <meta charset="UTF-8" />
+  </head>
+  <link rel="stylesheet" href="src/styles.css">
+  <body>
+    <canvas id="canvas"></canvas>
+    <div class="scale-contoller">
+      <div class="scale-contoller-item reset">0</div>
+      <div class="scale-contoller-item magnify">+</div>
+      <div class="scale-contoller-item minify">-</div>
+    </div>
+    <script src="src/index.js" type="module"></script>
+  </body>
+</html>
