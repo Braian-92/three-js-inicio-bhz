adea36ee08a714b3ae078eca8350ab7ac475b863
Braian-92 <zamudiobraianhernan@gmail.com>
2023-05-08 02:06:27 -0300

    test raycaster from hex mesh

---
+++ index26_hex_grid.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>HexGrid</title>
+  </head>
+  <body>
+    <canvas id="canvas" width="800" height="500"></canvas>
+    <script src="main.js"></script>
+  </body>
+</html>
+<script type="text/javascript">
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  const a = 2 * Math.PI / 6;
+  const r = 10;
+
+  function init() {
+    drawGrid(canvas.width, canvas.height);
+  }
+  init();
+
+  function drawGrid(width, height) {
+    for (let y = r, j = 0; y + r * Math.sin(a) < height; y += 2 ** ((j + 1) % 2) * r * Math.sin(a), j = 0) {
+      for (let x = r; x + r * (1 + Math.cos(a)) < width; x += r * (1 + Math.cos(a)), y += (-1) ** j++ * r * Math.sin(a)) {
+        drawHexagon(x, y);
+      }
+    }
+  }
+
+  function drawHexagon(x, y) {
+    ctx.beginPath();
+    for (let i = 0; i < 6; i++) {
+      ctx.lineTo(x + r * Math.cos(a * i), y + r * Math.sin(a * i));
+    }
+    ctx.closePath();
+    ctx.stroke();
+  }
+
+</script>
